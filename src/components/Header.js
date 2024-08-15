4ad7991218ed646aa54b616fcc9feefc2bc44072
Aman Raj <97093927+iamanrajput@users.noreply.github.com>
2024-08-15 16:45:20 +0530

    You Eat

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+
+const Header = () => {
+
+    const [btnName, setBtnName] = useState("👀Log In");
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={LOGO_URL} />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>🏠Home</li>
+                    <li>👤About Us</li>
+                    <li>📞Contact Us</li>
+                    <li>🛒Cart</li>
+                    <button 
+                        className="login" 
+                        onClick={() => {
+                            btnName === "👀Log In"
+                        ? setBtnName("👋 Log Out")
+                        : setBtnName("👀Log In");
+                        }}
+                    >
+                    {btnName}
+                </button>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
